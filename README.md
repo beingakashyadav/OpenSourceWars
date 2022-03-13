12af8db0ce92a848df247bab92ff4cdea53dbf72
Agustín Kanashiro <akanashiro@protonmail.com>
2022-03-13 17:23:14 -0300

    Clean up comments

--- README.md
+++ README.md
@@ -1,15 +1,47 @@
 # OpenSourceWars
 
-Este es un ejercicio para obtener la certificación del curso "Programación de contratos inteligentes con Solidity" de Blockchain Academy Chile
-Se deben cumplir las siguientes condiciones
+Este es un ejercicio para obtener la certificación del curso "Programación de contratos inteligentes con Solidity" de Blockchain Academy Chile.
+Se deben cumplir las siguientes condiciones:
 
 - [x] Seguridad por medio de permisos (que haya distintos permisos para los tipos de usuario)
-- [x] Crear una función que reciba pagos en ether
-- [X] Consumir alguna librería externa o heredar de un contrato externo (ej: OpenZeppelin)
-- [x] Desplegar los contratos en testnet. (Ej. Rinkeby)
-  - [ ] *En mi caso está desplegado en Kovan.*
-- [x] Conexión con oráculo. (Ej. Chainlink)
-- [ ] Habilitar una página web pública que nos permita interactuar con la aplicación usando Metamask. Pronto en https://kanashiro.me
-- [ ] Enlace para interactuar con la aplicación.
+- [x] Crear una función que reciba pagos en Ether.
+- [x] Consumir alguna librería externa o heredar de un contrato externo (ej: OpenZeppelin). 
+  - [x] *He usado Ownable.sol*
+- [x] Desplegar los contratos en testnet. (Ej. Rinkeby). 
+  - [x] *He usado Kovan*.
+- [x] Conexión con oráculo. (Ej. Chainlink). 
+  - [x] He usado:
+    - [x] VRFConsumerBase.sol
+    - [x] ConfirmedOwner.sol
+- [ ] Habilitar una página web pública que nos permita interactuar con la aplicación usando Metamask.
+- [ ] Enlace para interactuar con la aplicación. Pronto en [https://kanashiro.me](https://kanashiro.me)
 - [x] Repositorio de la aplicación en Github
 - [ ] Dirección de los contratos inteligentes desplegados en Rinkeby
+
+---
+
+This is an exercise to obtain the certification of Blockchain Academy Chile's "Smart contracts programming with Solidity".
+To achieve this, some conditions must be accomplished:
+
+- [x] Security by permissions (must be different permissions for every kind of users)
+
+- [x] Create a function to receive payments in Ether.
+
+- [x] Make us of any external library or inherit from an external contract (eg: OpenZeppelin).
+  
+  - [x] *I have used Ownable.sol*
+
+- [x] Deploy contracts in testnet. (Eg: Rinkeby).
+  
+  - [x] *I have used Kovan*.
+
+- [x] Connect to an oracle. (Eg: Chainlink)
+  
+  - [x] He usado:
+    
+    - [ ]     - [x] VRFConsumerBase.sol
+    - [x] ConfirmedOwner.sol
+- [ ] Enable a web page in order to interact with the application using Metamask. 
+- [ ] Link to interact with the application. Soon: [https://kanashiro.me](https://kanashiro.me)
+- [x] Github repo.
+- [ ] Smart contract address deployed in testnet.
--- contracts/OpenSourceWars.sol
+++ contracts/OpenSourceWars.sol
@@ -8,30 +8,32 @@ import "./RollDice.sol";
 
 contract OpenSourceWars is Ownable {
 
+    /**
+    * @dev un dado 
+    * @dev a dice
+    */
     VRFD20 public unDado;
 
-    // El último ganador de la pelea
+    /**
+    * @dev El último ganador de la pelea
+    * @dev Last match winner
+    */
     address public lastWinner;
 
-    /** @dev compradores */
+    /**
+    * @dev compradores
+    * @dev buyers
+    */
     mapping(address => uint) public buyers;
 
-    /**
-    * @dev Se elige un struct para el luchador porque se puede hacer evolucionar ciertas
-    * @dev características como velocidad y dificultad (facilidad de uso)
-    */
-    /*
-    struct Fighter {
-        uint idFighter;
-        uint strength; // maximo 1000
-        uint difficulty; // maximo 0 
-    } 
-    */
 
     address[6] fighterOwners;
     mapping(address => uint) public fighterMapping;  // es necesario manejar esto?
 
-    /* @dev Eventos */
+    /**
+    * @dev Eventos
+    * @dev Eventos
+    */
     event FighterBought(uint aFighter);
 
     /* =============================================================*/
@@ -43,8 +45,7 @@ contract OpenSourceWars is Ownable {
 
     /**
     * @dev Getters
-     */
-
+    */
     function getBalance() public view returns(uint) {
         return address(this).balance;
     }
@@ -68,14 +69,15 @@ contract OpenSourceWars is Ownable {
     }
 
     /**
-    * @dev Se elige un luchador a comprar. Se pasan parámetros porque la idea es 
-    * @dev almacenar la información para poder hacer evolucionar al luchador
-    * @param _idFighter: es el luchador
-    * @param _price: es el precio del luchador
+    * @dev Se elige un luchador a comprar.
+    * @dev A fighter is chosen.
+    * @param _idFighter: es el luchador.
+    * @param _price: es el precio del luchador.
     */
     function chooseFighter(uint _idFighter, uint _price) public returns (uint){
         /**
         * @dev Debe elegir un luchador válido
+        * @dev Must choose a fighter
          */
         require (_idFighter >= 0 && _idFighter <= 5, "Choose your fighter");
 
@@ -83,7 +85,8 @@ contract OpenSourceWars is Ownable {
         /**
         * @dev No puede comprar un luchador que ya compró.
         * @dev Tal vez esto se podría evitar si hago el control previamente en la página.
-         */
+        * @dev You can't buy a fighter already bought. This also can be controlled in weg page.
+        */
         require (fighterOwners[_idFighter] == 0x0000000000000000000000000000000000000000, "Already taken");
 
         require(msg.sender != fighterOwners[fighterMapping[msg.sender]], "You can only buy one fighter");
@@ -100,23 +103,15 @@ contract OpenSourceWars is Ownable {
         
         fighterOwners[_idFighter] = msg.sender;
         fighterMapping[msg.sender] = _idFighter;
-        /*
-        fighterMapping[msg.sender] = Fighter({
-                                            idFighter: _idFighter,
-                                            strength: _strength,
-                                            difficulty: _difficulty
-                                            });
-        */
         emit FighterBought(_idFighter);
         return _idFighter;
     }
 
     /**
     * @dev Función de tirar el dado
+    * @dev Roll dice function
     */
     function rollDice(address _inAddress) public{
-        // tirar un dado
-
         bytes32 request;
         request = unDado.rollDice(_inAddress);
 
@@ -138,13 +133,12 @@ contract OpenSourceWars is Ownable {
 
 
     /**
-    * @dev Luchar. Se tira el dado 2 veces, una vez para la fuerza, la otra para la
-    * @dev dificultad. La fórmula de lucha es:
-    * @dev [Fuerza * dado - (Dificultad * dado /2)]
-    * @dev El que obtenga mayor puntaje es el ganador
-    * @dev Si empatan queda el último ganador y deberán luchar de nuevo
-    * @dev No se requiere el idFighter1 ya que se utiliza el address del msg.sender 
-    * @param _idFighter2: luchador enemigo
+    * @dev Luchar. Aquí la lógica es simple para no gastar mucho gas. La mayor parte del cálculo está en el JS.
+    * @dev No se requiere el idFighter1 ya que se utiliza el address del msg.sender
+    * @dev --------------
+    * @dev Fight. Logic si simple here in order to consume the less gas as posible. Most of the calculation is done by JS. 
+    * @dev idFighter1 is not required as we use msg.sender address
+    * @param _idFighter2: luchador enemigo. Enemy fighter
     */
     function fight(int256 result1, int256 result2,
                  uint _idFighter2) public {
@@ -166,7 +160,8 @@ contract OpenSourceWars is Ownable {
 
 
     /**
-    * @dev Como desarrollador, puedo transferirme todo dinero del juego a mi wallet
+    * @dev Como dueño del contrato y el juego, puedo transferirme todo dinero del juego a mi wallet.
+    * @dev As contract and game owner, I can transfer all the money to my wallet.
     * @param _toAccount: la dirección de mi wallet
     */
     function transferFundsToDeveloper(address payable _toAccount) public onlyOwner {
@@ -175,9 +170,13 @@ contract OpenSourceWars is Ownable {
     }
 
     /* @dev Recibo ETH de billeteras externas */
+    /* @dev Receive ETH from external wallets */
     receive() payable external{
 
-        /** @dev Se carga un array de compradores para poder comprar después los luchadores */
+        /**
+        * @dev Se carga un array de compradores para poder comprar después los luchadores
+        * @dev 
+        */
         buyers[msg.sender] += msg.value;
     }
 
